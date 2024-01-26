f780b4a9b5d741e634c373614d20993e39cc4944
Alejandro de Brito Fontes <aledbf@gmail.com>
2024-01-26 16:18:17 +0000

    Add support for arm64

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,35 +6,32 @@ useradd -m user
 su user
 
 # install go
-VERSION='1.14'
+VERSION='1.21.6'
 OS='linux'
-ARCH='amd64'
 
-curl -OL https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz
-tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
-rm go$VERSION.$OS-$ARCH.tar.gz
+ARCH=""
+case $(uname -m) in
+    i386)   ARCH="386" ;;
+    i686)   ARCH="386" ;;
+    x86_64) ARCH="amd64" ;;
+    arm)    dpkg --print-architecture | grep -q "arm64" && ARCH="arm64" || ARCH="arm" ;;
+esac
+
+curl -fsSL https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz | tar xzs -C /usr/local
 
 INSTALLED_GO_VERSION=$(go version)
 echo "Go version ${INSTALLED_GO_VERSION} is installed"
 
-# install gopls, dlv, hey
-echo "Getting development tools"
-go get -u golang.org/x/tools/gopls
-go get -u github.com/go-delve/delve/cmd/dlv
-go get -u github.com/rakyll/hey
-
 # vscode-go dependencies 
-echo "Getting dependencies for the vscode-go plugin "
-# via: https://github.com/microsoft/vscode-go/blob/master/.travis.yml
-go get -u -v github.com/acroca/go-symbols
-go get -u -v github.com/cweill/gotests/...
-go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
-go get -u -v github.com/haya14busa/goplay/cmd/goplay
-go get -u -v github.com/mdempsky/gocode
-go get -u -v github.com/ramya-rao-a/go-outline
-go get -u -v github.com/rogpeppe/godef
-go get -u -v github.com/sqs/goreturns
-go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
-go get -u -v github.com/zmb3/gogetdoc
-go get -u -v golang.org/x/lint/golint
-go get -u -v golang.org/x/tools/cmd/gorename
+echo "Getting dependencies for the vscode-go plugin"
+go install -v github.com/uudashr/gopkgs/cmd/gopkgs@v2 \
+    && go install -v github.com/ramya-rao-a/go-outline@latest \
+    && go install -v github.com/cweill/gotests/gotests@latest \
+    && go install -v github.com/fatih/gomodifytags@latest \
+    && go install -v github.com/josharian/impl@latest \
+    && go install -v github.com/haya14busa/goplay/cmd/goplay@latest \
+    && go install -v github.com/go-delve/delve/cmd/dlv@latest \
+    && go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@latest \
+    && go install -v golang.org/x/tools/gopls@latest \
+    && go install -v honnef.co/go/tools/cmd/staticcheck@latest \
+    && go install -v github.com/rakyll/hey@latest
